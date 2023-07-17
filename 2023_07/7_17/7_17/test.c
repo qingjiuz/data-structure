268eb87cf32d7097f8ae2a9369445a3e607dbbb4
qingjiuz <2900105829@qq.com>
2023-07-17 23:53:19 +0800

    单链表oj练习

---
+++ 2023_07/7_17/7_17/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//nowcoder:CM11 Á´±í·Ö¸î
+//ListNode* partition(ListNode* pHead, int x) {
+//    if (pHead == NULL)
+//        return NULL;
+//
+//    ListNode* n1 = (ListNode*)malloc(sizeof(ListNode));
+//    ListNode* n2 = (ListNode*)malloc(sizeof(ListNode));
+//    ListNode* cur = pHead, * head1 = n1, * head2 = n2;
+//
+//    n1->next = NULL;
+//    n2->next = NULL;
+//
+//    while (cur)
+//    {
+//        if (cur->val < x)
+//        {
+//            n1->next = cur;
+//            n1 = n1->next;
+//        }
+//        else
+//        {
+//            n2->next = cur;
+//            n2 = n2->next;
+//        }
+//        cur = cur->next;
+//    }
+//
+//    n1->next = head2->next;
+//    n2->next = NULL;
+//
+//    return head1->next;
+//}
+
+int main()
+{
+
+	return 0;
+}
